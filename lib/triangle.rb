9cfac458def39436777b14c23dc4d3ac48e71667
Samuel Hinga <samuelhinga200@gmail.com>
2022-06-12 00:12:04 +0300

    commit tree 8592e19281917c7927c486c512e4476759631110

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  attr_reader :l1, :l2, :l3
+
+  def initialize l1, l2, l3
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    validate_triangle
+    if l1 == l2 && l2 ==l3
+      :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [l1, l2, l3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
